6f653b80c562b17e8a6ccebb34183bf1384de6c6
lucymn111 <160658241+lucymn111@users.noreply.github.com>
2024-03-07 16:28:26 +0300

    Update Control Flow Assignment.dart
    Decision making statement added

--- Control Flow Assignment.dart
+++ Control Flow Assignment.dart
@@ -16,4 +16,18 @@ void main() {
   if (a == 10) {
     print('Your number is equal to 10');
   }
+
+  //statement examples
+  print('Enter a number:');
+  int number = 10;
+
+  if (number > 10) {
+    print('Your number is greater than 10');
+  } else if (number < 10) {
+    print('Your number is less than 10');
+  } else if (number == 10) {
+    print('Your number is equal to 10');
+  } else {
+    print('Invalid input');
+  }
 }

b362e0261f63bd4ab58b150c201428a1a51de6d6
lucymn111 <160658241+lucymn111@users.noreply.github.com>
2024-03-07 13:46:07 +0300

    Add files via upload

---
+++ Control Flow Assignment.dart
@@ -0,0 +1,19 @@
+void main() {
+  //If the number is greater than 10, print "Your number is greater than 10"
+  int i = 12;
+  if (i > 10) {
+    print('Your number is greater than 10');
+  }
+
+  //If the number is less than 10, print "Your number is less than 10"
+  int x = 8;
+  if (x < 10) {
+    print('Your number is less than 10');
+  }
+
+  //If the number is equal to 10, print "Your number is equal to 10"
+  int a = 10;
+  if (a == 10) {
+    print('Your number is equal to 10');
+  }
+}
